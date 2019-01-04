8cd99ae36e9454c1cd584a7075f778eeeab398ac
DuXawm22 <prawiravincent98@gmail.com>
2019-01-04 19:33:44 +0700

    adding move function,directions

---
+++ othello.py
@@ -0,0 +1,58 @@
+EMPTY, BLACK, WHITE, OUTER = '.', '@', 'o', '?'
+PIECES = (EMPTY, BLACK, WHITE, OUTER)
+PLAYERS = {BLACK: 'Black', WHITE: 'White'}
+
+UP, DOWN, LEFT, RIGHT = -10, 10, -1, 1
+UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT = -9, 11, 9, -11
+DIRECTIONS = (UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT, UP_LEFT)
+
+def squares():
+    return [i for i in xrange(11, 89) if 1 <= (i % 10) <= 8]
+
+def initial_board():
+    board = [OUTER] * 100
+    for i in squares():
+        board[i] = EMPTY
+    board[44], board[45] = WHITE, BLACK
+    board[54], board[55] = BLACK, WHITE
+    return board
+
+def print_board(board):
+    rep = ''
+    rep += '  %s\n' % ' '.join(map(str, range(1, 9)))
+    for row in xrange(1, 9):
+        begin, end = 10*row + 1, 10*row + 9
+        rep += '%d %s\n' % (row, ' '.join(board[begin:end]))
+    return rep
+
+def make_move(move, player, board):
+    board[move] = player
+    for d in DIRECTIONS:
+        make_flips(move, player, board, d)
+    return board
+
+def make_flips(move, player, board, direction):
+
+    bracket = find_bracket(move, player, board, direction)
+    if not bracket:
+        return
+    square = move + direction
+    while square != bracket:
+        board[square] = player
+        square += direction
+
+class IllegalMoveError(Exception):
+
+    def __init__(self, player, move, board):
+        self.player = player
+        self.move = move
+        self.board = board
+
+    def __str__(self):
+        return '%s cannot move to square %d' % (PLAYERS[self.player], self.move)
+
+def legal_moves(player, board):
+    return [sq for sq in squares() if is_legal(sq, player, board)]
+
+def any_legal_move(player, board):
+    return any(is_legal(sq, player, board) for sq in squares())
